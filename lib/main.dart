3203675f13f04a6a06a23de8769eadd48b94c752
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-07-18 17:20:57 +0530

    Added Sign Up Option.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:firebase_flutter/Setups/LoginPage.dart';
+
+import 'package:firebase_flutter/Pages/Setups/LoginPage.dart';
+import 'package:firebase_flutter/Pages/Setups/welcome.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: WelcomePage(),
     );
   }
 }
